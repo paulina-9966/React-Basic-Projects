d1a7fd1848c17c2b4d0d8b899e5a23d2c46275b9
paulina-9966 <61942509+paulina-9966@users.noreply.github.com>
2021-02-11 23:45:12 +0100

    Add files via upload

---
+++ project-15-cocktails-menu/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p> ljbvids avbiabic habihbahib cahsbiasb jkasfgitw
+        vjsbhb vjdb  hadbv  hdbvhdbs v ibddhs vhv vbvhvbhsdv  v hbhvdsv 
+      </p>
+    </section>
+  )
+}
+
+export default About
